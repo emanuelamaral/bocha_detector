535e8698e10fa075a16909821f86b0985fafa4cf
emanuel <amoz_c.p.t@hotmail.com>
2023-09-09 19:24:44 -0300

    Code refactoring

--- bocha_detector.py
+++ bocha_detector.py
@@ -7,28 +7,35 @@ class BochaDetector:
         self.img = cv2.imread(image_path)
         self.threshold_value = 127
         self.canny_threshold = 100
+        self.gray_img = cv2.cvtColor(self.img.copy(), cv2.COLOR_RGB2GRAY)
 
     def on_trackbar_threshold(self, value):
         self.threshold_value = value
-        self.canny_threshold = value
 
-        gray_img = cv2.cvtColor(self.img.copy(), cv2.COLOR_RGB2GRAY)
-
-        # blurred_img = cv2.GaussianBlur(gray_img, (5, 5), 0)
-        # canny_img = cv2.Canny(blurred_img, self.canny_threshold, self.canny_threshold * 2)
-        #
-        # cv2.imshow("Imagem com Filtro Canny", canny_img)
-
-        _, binary_img_gray_scale = cv2.threshold(gray_img, self.threshold_value, 255, cv2.THRESH_TOZERO_INV)
+        _, binary_img_gray_scale = cv2.threshold(self.gray_img, self.threshold_value, 255, cv2.THRESH_TOZERO_INV)
         cv2.imshow('Imagem Binarizada em Escala de Cinza', binary_img_gray_scale)
 
         circles = self.detect_circles(binary_img_gray_scale)
         img_with_circles = self.draw_circles(self.img.copy(), circles)
 
-        cv2.imshow("Circulos", img_with_circles)
+        cv2.imshow("Circulos - Threshold", img_with_circles)
 
         # _, binary_img = cv2.threshold(self.img, self.threshold_value, 255, cv2.THRESH_BINARY)
         # cv2.imshow('Imagem Binarizada com todos os canais', binary_img)
+
+    def on_trackbar_canny(self, value):
+        self.canny_threshold = value
+
+        # blurred_img = cv2.GaussianBlur(self.gray_img, (5, 5), 0)
+        canny_img = cv2.Canny(self.gray_img, self.canny_threshold, self.canny_threshold * 2)
+
+        cv2.imshow("Imagem com Filtro Canny", canny_img)
+
+        circles = self.detect_circles(canny_img)
+        img_with_circles = self.draw_circles(self.img.copy(), circles)
+        #
+        cv2.imshow("Circulos - Canny", img_with_circles)
+
 
     @staticmethod
     def detect_circles(binary_img):
@@ -49,7 +56,7 @@ class BochaDetector:
     def run(self):
         cv2.imshow('Imagem Original', self.img)
         cv2.createTrackbar('Threshold', 'Imagem Original', self.threshold_value, 255, self.on_trackbar_threshold)
-        cv2.createTrackbar('Canny Threshold', 'Imagem Original', self.canny_threshold, 200, self.on_trackbar_threshold)
+        cv2.createTrackbar('Canny Threshold', 'Imagem Original', self.canny_threshold, 200, self.on_trackbar_canny)
 
         while True:
             key = cv2.waitKey(1) & 0xFF
